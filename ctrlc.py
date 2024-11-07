eb2c3892cb4ff38c4fcb985e62a24c34208a306c
Carlos Maldonado <maldonadoviveroscarlos@gmail.com>
2024-11-06 19:45:49 -0600

    second_commit

--- ctrlc.py
+++ ctrlc.py
@@ -2,13 +2,12 @@ def keyboard_shortcut(text):
     clipboard, copied_text, i = [], '', 0
     while i < len(text.split()):
         current_word = text.split()[i]
-        if i != len(text.split())-1 and current_word == "Ctrl":
+        if i != len(text.split())-1 and ''.join(text.split()[i:i+3]) == "Ctrl+C" or ''.join(text.split()[i:i+3]) == "Ctrl+V":
             command = ''.join(text.split()[i:i+3])
-            if command == "Ctrl+C" or command == "Ctrl+V":
-                if command == "Ctrl+C": 
-                    copied_text = ' '.join(clipboard)
-                elif command == "Ctrl+V": 
-                    clipboard += copied_text.split()
+            if command == "Ctrl+C": 
+                copied_text = ' '.join(clipboard)
+            elif command == "Ctrl+V": 
+                clipboard += copied_text.split()
             i += 3
         else: 
             clipboard.append(current_word)
@@ -18,6 +17,6 @@ def keyboard_shortcut(text):
 def main():
     text = "The Ctrl + C Ctrl + V Town Ctrl + C Ctrl + V"
     print(keyboard_shortcut(text))
-main()
 
-#the egg and the egg and the spoon
+if __name__ == "__main__":
+    main()
